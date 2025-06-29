2bb407932a478b6774a9e023e28b12cb8c3147ca
iferdel <ignaferdel@gmail.com>
2025-06-29 19:28:30 -0400

    listen and serve

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
 func main() {
+	const port = "8080"
+
+	mux := http.NewServeMux() // serve mux is not a multiplexor per-se but a traffic director
+	server := http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
+	fmt.Printf("serving on port %s\n", port)
+	log.Fatal(server.ListenAndServe())
+
 	return
 }

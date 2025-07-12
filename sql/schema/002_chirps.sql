58684ec5815ae8eed5f8b0f0dadc75543d1a8a96
iferdel <ignaferdel@gmail.com>
2025-07-12 15:19:51 -0400

    chirp table

---
+++ sql/schema/002_chirps.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+CREATE TABLE chirps (
+	id UUID PRIMARY KEY,
+	created_at TIMESTAMPTZ NOT NULL,
+	updated_at TIMESTAMPTZ NOT NULL,
+	body TEXT NOT NULL,
+	user_id UUID NOT NULL,
+	CONSTRAINT fk_user
+		FOREIGN KEY(user_id)
+			REFERENCES users(id)
+);
+
+-- +goose Down
+DROP TABLE chirps;
+

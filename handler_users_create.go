2c23cd4405efe4212888c956ffe4b829f7f690c2
iferdel <ignaferdel@gmail.com>
2025-07-13 07:45:51 -0400

    create user with hashed password funcionality

--- handler_users_create.go
+++ handler_users_create.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"net/http"
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/iferdel-vault/bootdev-http-servers/internal/auth"
+	"github.com/iferdel-vault/bootdev-http-servers/internal/database"
 )
 
 type User struct {
@@ -20,11 +23,14 @@ func (apiCfg *apiConfig) handlerUsersCreate(w http.ResponseWriter, r *http.Reque
 	defer r.Body.Close()
 
 	type requestBody struct {
-		Email          string `json:"email"`
-		HashedPassword string `json:"hashed_password"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
 	}
 	type responseBody struct {
-		User
+		ID        uuid.UUID `json:"id"`
+		CreatedAt time.Time `json:"created_at"`
+		UpdatedAt time.Time `json:"updated_at"`
+		Email     string    `json:"email"`
 	}
 
 	decoder := json.NewDecoder(r.Body)
@@ -35,18 +41,29 @@ func (apiCfg *apiConfig) handlerUsersCreate(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
-	user, err := apiCfg.db.CreateUser(r.Context(), params.Email)
+	hashedPassword, err := auth.HashPassword(params.Password)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Password entered not valid", err)
+		return
+	}
+
+	user, err := apiCfg.db.CreateUser(r.Context(), database.CreateUserParams{
+		Email: params.Email,
+		HashedPassword: sql.NullString{
+			String: hashedPassword,
+			Valid:  true,
+		},
+	})
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Couldn't create user", err)
 		return
 	}
 
 	respondWithJSON(w, http.StatusCreated, responseBody{
-		User: User{
-			ID:        user.ID,
-			CreatedAt: user.CreatedAt,
-			UpdatedAt: user.UpdatedAt,
-			Email:     user.Email,
-		},
-	})
+		ID:        user.ID,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+		Email:     user.Email,
+	},
+	)
 }

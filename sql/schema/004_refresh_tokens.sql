fae25aa281f09ebb638104548ecd28b40ed955ec
iferdel <ignaferdel@gmail.com>
2025-07-14 19:55:09 -0400

    refres_tokens table

---
+++ sql/schema/004_refresh_tokens.sql
@@ -0,0 +1,16 @@
+-- +goose Up
+CREATE TABLE refresh_tokens (
+	token TEXT PRIMARY KEY,
+	created_at TIMESTAMPTZ NOT NULL,
+	updated_at TIMESTAMPTZ NOT NULL,
+	user_id UUID NOT NULL,
+	expires_at TIMESTAMPTZ,
+	revoked_at TIMESTAMPTZ DEFAULT NULL,
+	CONSTRAINT fk_user
+		FOREIGN KEY(user_id)
+			REFERENCES users(id)
+			ON DELETE CASCADE
+);
+
+-- +goose Down
+DROP TABLE refresh_tokens;

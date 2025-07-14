15ca634f0f18f0504fbdcc0eea732e779a21d3f5
iferdel <ignaferdel@gmail.com>
2025-07-14 08:56:20 -0400

    create chirp now asks for auth

--- handler_chirps_create.go
+++ handler_chirps_create.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/iferdel-vault/bootdev-http-servers/internal/auth"
 	"github.com/iferdel-vault/bootdev-http-servers/internal/database"
 )
 
@@ -39,16 +40,27 @@ func (cfg *apiConfig) handlerChirpsCreate(w http.ResponseWriter, r *http.Request
 	defer r.Body.Close()
 
 	type requestBody struct {
-		Body   string    `json:"body"`
-		UserID uuid.UUID `json:"user_id"`
+		Body string `json:"body"`
 	}
 	type responseBody struct {
 		Chirp
 	}
 
+	tokenString, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Something went wrong with the token verification from the header content of the request", err)
+		return
+	}
+
+	userID, err := auth.ValidateJWT(tokenString, cfg.jwtSecret)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Unauthorized...", err)
+		return
+	}
+
 	decoder := json.NewDecoder(r.Body)
 	params := requestBody{}
-	err := decoder.Decode(&params)
+	err = decoder.Decode(&params)
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Something went wrong. Probably no chirp was stated in the body message", err)
 		return
@@ -62,7 +74,7 @@ func (cfg *apiConfig) handlerChirpsCreate(w http.ResponseWriter, r *http.Request
 	cleanedBody := BadWordReplacement(params.Body)
 	chirp, err := cfg.db.CreateChirp(r.Context(), database.CreateChirpParams{
 		Body:   cleanedBody,
-		UserID: params.UserID,
+		UserID: userID,
 	})
 
 	respondWithJSON(w, http.StatusCreated, responseBody{
@@ -71,7 +83,7 @@ func (cfg *apiConfig) handlerChirpsCreate(w http.ResponseWriter, r *http.Request
 			CreatedAt: chirp.CreatedAt,
 			UpdatedAt: chirp.UpdatedAt,
 			Body:      chirp.Body,
-			UserID:    chirp.UserID,
+			UserID:    userID,
 		},
 	})
 	return

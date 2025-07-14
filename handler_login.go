e7f8982eff873d4d0a706145d5ac8f92c4f6946b
iferdel <ignaferdel@gmail.com>
2025-07-14 09:01:39 -0400

    expiration time should be an int to be parsed correctly to show secons in json and not time.duration

--- handler_login.go
+++ handler_login.go
@@ -14,13 +14,14 @@ func (cfg *apiConfig) handlerLogin(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
 	type requestBody struct {
-		Email            string         `json:"email"`
-		Password         string         `json:"password"`
-		ExpiresInSeconds *time.Duration `json:"expires_in_seconds"`
+		Email            string `json:"email"`
+		Password         string `json:"password"`
+		ExpiresInSeconds int    `json:"expires_in_seconds"`
 	}
 	type responseBody struct {
 		User
-		Token string `json:"token"`
+		Token        string `json:"token"`
+		RefreshToken string `json:"refresh_token"`
 	}
 
 	params := requestBody{}
@@ -43,14 +44,16 @@ func (cfg *apiConfig) handlerLogin(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	expiresIn := time.Hour
-	if params.ExpiresInSeconds != nil {
-		if *params.ExpiresInSeconds < time.Hour {
-			expiresIn = *params.ExpiresInSeconds
-		}
+	expirationTime := time.Hour
+	if params.ExpiresInSeconds > 0 && params.ExpiresInSeconds < 3600 {
+		expirationTime = time.Duration(params.ExpiresInSeconds) * time.Second
 	}
 
-	jwtToken, err := auth.MakeJWT(user.ID, cfg.jwtSecret, expiresIn)
+	jwtToken, err := auth.MakeJWT(user.ID, cfg.jwtSecret, expirationTime)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't create access JWT", err)
+		return
+	}
 
 	respondWithJSON(w, http.StatusOK, responseBody{
 		User: User{
